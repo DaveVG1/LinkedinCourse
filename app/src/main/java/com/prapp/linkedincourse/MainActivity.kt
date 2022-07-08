2253422a84bec16e34978c913354dbf932e18d62
David Vidriales Guerrero <vgdavid91@gmail.com>
2022-07-08 14:52:29 +0200

    Initial commit

---
+++ app/src/main/java/com/prapp/linkedincourse/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.prapp.linkedincourse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
